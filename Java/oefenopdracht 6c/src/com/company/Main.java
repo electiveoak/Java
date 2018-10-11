525fe3f0a64773da2ce8e50afa764e9a8502e95a
electiveoak <39827820+electiveoak@users.noreply.github.com>
2018-10-11 12:35:27 +0200

    Add files via upload
    files geupload

---
+++ Java/oefenopdracht 6c/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public static int[] genereerGetallen(){
+
+    }
+    public static boolean KomtVoorIn(int zoekgetal, int[] lijst){
+
+    }
+}
